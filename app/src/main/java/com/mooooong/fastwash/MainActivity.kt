58f6c38764a12ecc6bc9bfab1b1ca34466dcd2e2
최민재 <cmjcmj1230@gmail.com>
2023-03-31 10:53:15 +0900

    ⚡️ :: add get device name

--- app/src/main/java/com/mooooong/fastwash/MainActivity.kt
+++ app/src/main/java/com/mooooong/fastwash/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.mooooong.fastwash
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,37 +11,33 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.mooooong.fastwash.ui.theme.FastwashTheme
 
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
         setContent {
             FastwashTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    if (ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.BLUETOOTH_CONNECT
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        Text(text = bluetoothAdapter.name ?: "응애")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    FastwashTheme {
-        Greeting("Android")
     }
 }
